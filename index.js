80e1d06ec492ed391832c137af6d22909b9d4df0
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-12-10 19:38:43 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+const prefix = "!"
+
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        Discord.Intents.FLAGS.GUILD_INVITES,
+        Discord.Intents.FLAGS.GUILD_BANS,
+    ],
+    partials: ["CHANNEL"]
+});
+client.commands = new Discord.Collection();
+client.commands.normal = new Discord.Collection();
+client.events = new Discord.Collection();
+client.commands.normal.aliases = new Discord.Collection();
+client.commands.buttons = new Discord.Collection();
+client.commands.menus = new Discord.Collection();
+client.commands.slash = new Discord.Collection();
+
+const eventFiles = fs.readdirSync('./listeners').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./listeners/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.run(...args));
+	} else {
+		client.on(event.name, (...args) => event.run(...args));
+	}
+}
+
+client.on("ready", () => {
+	console.log('Bot is now online')
+})
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return message.reply("Non puoi usare il bot in una chat privata");
+    if(!message.guild) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd.length == 0) return;
+    var command = client.commands.get(cmd);
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
+    if(command) {
+        command.run(client,message,args);
+    }
+});
+
+client.login('');
