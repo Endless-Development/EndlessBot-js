c9e693118a89ea1f9c54b7c6822c9f63fe5d0ea5
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-12-11 00:13:17 +0100

    Update ready.js

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,9 +1,7 @@
-const chalk = require("chalk");
-
 module.exports = {
     name: 'ready',
     once: true,
     run: async(client, message, args) => {
-        console.log(chalk.green("[INFO] Bot Successful connected to Discord."));
+        console.log("[INFO] Bot is now online!");
     }
 }
