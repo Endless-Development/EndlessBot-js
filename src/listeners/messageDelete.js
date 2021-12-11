82287d387a2c41a6074b2771248912bebee8d3ec
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-12-11 15:45:32 +0100

    Update messageDelete.js

--- src/listeners/messageDelete.js
+++ src/listeners/messageDelete.js
@@ -1,17 +1,20 @@
-
 const { MessageEmbed } = require("discord.js");
 const index = require("../../index");
 
 module.exports = {
     name: 'messageDelete',
     run: async(message) => {
+        if(!index.client.users.cache.has(message.author.id)) {
+            console.log(message.author.tag+" | ID: "+message.author.id+" has been added to the user cache")
+            index.client.users.cache.set(message.author.id, message.author);
+        }
+        var user = index.client.users.cache.get(message.author.id);
+
         let embed = new MessageEmbed()
-        .setTitle(`**Messaggio di ${message.member} eliminato in ${message.channel}**`)
-        .setAuthor(message.author.tag, message.author.avatarURL)
         .setColor("#ff0000")
         .addField(`Contenuto:`, `\`\`\`${message.content}\`\`\``)
         .setFooter(`ID Autore: ${message.author.id} | ID Messaggio: ${message.id}`);
     
-        index.client.channels.cache.find(channel => channel.id == index.messageLogChannel).send({ embeds: [embed] });
+        index.client.channels.cache.find(channel => channel.id == index.messageLogChannel).send({ content: `**Messaggio di ${user} eliminato in ${message.channel}**`, embeds: [embed] });
     }
 }
