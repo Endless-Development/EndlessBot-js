5bce785020bac8b3bb74913d7fe2e162b825594e
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-12-11 18:35:21 +0100

    Create channelUpdate.js

---
+++ src/listeners/channelUpdate.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js");
+const index = require("../../index");
+
+module.exports = {
+    name: 'channelUpdate',
+    run: async(old, channel) => {
+        if(channel.partial) return;
+
+        let embed = new MessageEmbed()
+        .setTitle("Canale modificato")
+        .setFooter(`ID Canale: ${channel.id}`);
+
+        if(old.name != channel.name) {
+            embed.addField(`Nome prima:`, `\`\`\`${old.name}\`\`\``, true);
+            embed.addField(`Nome dopo:`, `\`\`\`${channel.name}\`\`\``, true);
+        }
+        
+        if(channel.isVoice()) embed.addField(`Tipologia:`, `\`\`\`Canale vocale\`\`\``);
+        if(channel.isText()) embed.addField(`Tipologia:`, `\`\`\`Canale testuale\`\`\``);
+        
+        if(old.parent != channel.parent && channel.parent != null) {
+            embed.addField(`Categoria prima:`, `\`\`\`${old.parent.name}\`\`\``, true);
+            embed.addField(`Categoria dopo:`, `\`\`\`${channel.parent.name}\`\`\``, true);
+        }
+
+        if((old.permissionOverwrites != channel.permissionOverwrites) || (old.position != role.position)) {
+            embed.addField(`Permessi o posizione modificata:`, `\`\`\`Sì\`\`\``);
+        }
+
+        if(embed.fields.count <= 1) return;
+
+        index.client.channels.cache.find(channel => channel.id == index.channelLogChannel).send({ content: `**${role}**`, embeds: [embed] });
+    }
+}
