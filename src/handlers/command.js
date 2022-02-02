1db24bcb3251d34f884f28379ef44de910918fc9
Kheeto <70845116+Kheeto@users.noreply.github.com>
2022-02-02 09:28:09 +0100

    Update command.js

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,21 +1,49 @@
+const { token, clientId, guildId } = require("../../config/main.json");
 const { readdirSync } = require("fs");
 const { client } = require("../../index");
+const Logger = require("../util/Logger");
 var commands;
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+var slashCommands = [];
+
 function run() {
     console.log("[HANDLER] Loading Commands...");
     commands = readdirSync(`./src/commands`).filter(file => file.endsWith(".js"));
 
     for(let file of commands) {
         pull = require(`../commands/${file}`);
+
+        if(pull.slash) {
+            slashCommands.push(pull.data.toJSON());
+        }
+
         if(pull.name) {
             client.commands.set(pull.name, pull);
-            console.log("[COMMAND] Loaded Command "+ pull.name);
+            Logger.Info("[COMMAND] Loaded Command "+ pull.name);
         } else {
-            console.log("[ERROR] Error while loading "+pull+", couldn't find the command name.")
+            Logger.Error("[ERROR] Error while loading "+pull+", couldn't find the command name.")
             continue;
         }
         if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
     }
+
+    const rest = new REST({ version: '9' }).setToken(token);
+    (async () => {
+        try {
+            Logger.Info('Loading Slash commands.');
+
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                { body: slashCommands },
+            );
+
+            Logger.Success('Successfully loaded Slash commands.');
+        } catch (error) {
+            Logger.Error(error);
+        }
+    })();
 }
 
 module.exports = {
