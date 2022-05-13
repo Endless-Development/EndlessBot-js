53d35a99efba8b6d8056da09a755c6a68f01d4cf
Kheeto <70845116+Kheeto@users.noreply.github.com>
2022-05-13 18:33:56 +0200

    Create ping.js

---
+++ src/slashCommands/ping.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    run: async (interaction) => {
+        const msg = await interaction.channel.send("<@"+interaction.member.id+">, Sto caricando il mio ping...");
+
+        const ping = `${Math.floor(msg.createdTimestamp - interaction.createdTimestamp)}ms`
+
+        const embedPing = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("Ping del bot")
+            .setDescription(ping);
+        
+        msg.delete();
+        interaction.reply({ embeds: [embedPing] });
+    }
+}
