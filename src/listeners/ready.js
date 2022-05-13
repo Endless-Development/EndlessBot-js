b0d4e0b7e25c12e8ed533f8cbac21665b50cc731
Kheeto <70845116+Kheeto@users.noreply.github.com>
2022-05-13 18:29:49 +0200

    Update ready.js

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,9 +1,13 @@
 const Logger = require("../util/Logger");
+const slashCommands = require("../util/slashCommandsUtils")
+const { client } = require("../../index");
 
 module.exports = {
     name: 'ready',
     once: true,
     run: function() {
         Logger.Success("[INFO] Bot is now online!");
+
+        slashCommands.createSlashCommands('885121885346623498', client);
     }
 }
