f14c2cce86d0ac0a6185c3a6dce2b9663bfd13e1
Kheeto <70845116+Kheeto@users.noreply.github.com>
2022-05-15 00:25:39 +0200

    Update timeout.js

--- src/slashCommands/timeout.js
+++ src/slashCommands/timeout.js
@@ -1,9 +1,10 @@
-const { MessageEmbed, User } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
 
 module.exports = {
     name: "timeout",
     run: async (interaction) => {
-        if(!interaction.member.permissions.has("MODERATE_MEMBERS.")) {
+        if(!interaction.member.permissions.has("MODERATE_MEMBERS")) {
             interaction.reply({
                 content: "Non hai il permesso di usare questo comando!",
                 ephemeral: true
@@ -13,13 +14,23 @@ module.exports = {
 
         var utente = interaction.options.getUser("utente");
         var membro = interaction.guild.members.cache.get(utente.id);
-        var durata = interaction.options.getNumber("durata");
-        var tipoDurata = interaction.options.getString("tipologia");
+
+        if(!membro) {
+            interaction.reply({
+                content: "Non hai specificato un utente valido!",
+                ephemeral: true
+            })
+            return;
+        }
+
+        var durata = interaction.options.getString("durata");
         var motivo = interaction.options.getString("motivo") || "Nessun motivo specificato.";
 
-        if((tipoDurata != "secondi") && (tipoDurata != "minuti") && (tipoDurata != "ore") && (tipoDurata != "giorni")) {
+        var durataMS = ms(durata);
+
+        if(durataMS <= 0) {
             interaction.reply({
-                content: "Hai specificato un tipo di durata non valido! Devi scrivere secondi/minuti/ore/giorni",
+                content: "La durata del timeout deve essere superiore a 0 millisecondi!",
                 ephemeral: true
             })
             return;
@@ -33,33 +44,33 @@ module.exports = {
             return;
         }
 
-        if(!membro.moderatable) {
+        if(membro.roles.resolve("918903787320672357")) {
+            interaction.reply({
+                content: "Non puoi mettere in timeout un membro del Consiglio Federale",
+                ephemeral: true
+            })
+            return;
+        }
+
+        /*if(!membro.moderatable) {
             interaction.reply({
                 content: "Non posso mettere questo utente in timeout",
                 ephemeral: true
             })
             return;
-        }
+        }*/
 
         const embedPing = new MessageEmbed()
             .setColor("#ff0000")
             .setTitle("Timeout")
             .setDescription("Un utente è stato messo in timeout.")
             .addField("Utente in timeout", "<@"+utente.id+">", true)
-            .addField("Durata del timeout", durata + " " + tipoDurata, true)
+            .addField("Durata del timeout", durata, true)
             .addField("Motivazione", motivo, true)
             .addField("Messo in timeout da", "<@"+interaction.member.id+">", true)
-        
-        if(tipoDurata == "secondi") {
-            membro.timeout(durata*1000, motivo);
-        } else if(tipoDurata == "minuti") {
-            membro.timeout(durata*60*1000, motivo);
-        } else if(tipoDurata == "ore") {
-            membro.timeout(((durata*60)*60)/1000, motivo);
-        } else if(tipoDurata == "giorni") {
-            membro.timeout((((durata*60)*60)*24)/1000, motivo);
-        }
 
-        interaction.reply({ embeds: [embedPing] });
+        membro.timeout(durataMS, motivo);
+
+        await interaction.reply({ embeds: [embedPing] });
     }
 }
