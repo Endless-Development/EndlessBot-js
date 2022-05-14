27a6b96d1cf5953e5e3b6acab80fa1d897e5a27d
Kheeto <70845116+Kheeto@users.noreply.github.com>
2022-05-14 12:49:20 +0200

    Create help.js

---
+++ src/slashCommands/help.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+const pagination = require("discord.js-pagination");
+
+module.exports = {
+    name: "help",
+    run: async (interaction) => {
+        const pagina1 = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("Endless Network Bot")
+            .setDescription("Questo bot è stato creato da <@530817199632416778>, ha funzioni soprattutto per gli Staff, ma anche alcuni comandi per gli utenti, ha un sistema di log per tutto ciò che succede nel server, comandi slash, comandi di moderazione, e altre funzioni verranno aggiunte in futuro, per sostituire i bot presenti attualmente.")
+            .addField("Comandi del bot", "Vai alla pagina successiva per la lista di comandi.");
+
+        const pagina2 = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("Endless Network Bot")
+            .addField("/help", "Visualizza questa pagina con le info del bot.", true)
+            .addField("/ping", "Visualizza il ping del bot con questo server discord.", true)
+            .addField("/timeout", "Comando degli staff per mettere in timeout.", true)
+            .addField("Il bot è ancora in sviluppo", "Verranno aggiunti molti altri comandi", true)
+
+        var pages = [
+            pagina1,
+            pagina2,
+        ];
+        var emojis = ['⏪', '⏩']
+
+        var embedHelp = pagination(msg, pages, emojis, 120000);
+        interaction.reply({ embeds: [ embedHelp ] });
+    }
+}
