ca1dc6f0c229db8ac713a962bfc4ed1838e0d16c
Kheeto <70845116+Kheeto@users.noreply.github.com>
2022-05-14 15:14:50 +0200

    Update slashCommandsUtils.js

--- src/util/slashCommandsUtils.js
+++ src/util/slashCommandsUtils.js
@@ -12,7 +12,67 @@ function createSlashCommands(guildId, client) {
         {
             name: "help",
             description: "Visualizza le informazioni del bot, e la lista dei comandi"
-        }
+        },
+        {
+            name: "timeout",
+            description: "Mette un utente in timeout per il tempo specificato",
+            options: [
+                {
+                    name: "utente",
+                    description: "Chi stai mettendo in timeout?",
+                    type: "USER",
+                    required: true
+                },
+                {
+                    name: "durata",
+                    description: "Per quanto tempo starà in timeout?",
+                    type: "NUMBER",
+                    required: true
+                },
+                {
+                    name: "tipologia",
+                    description: "secondi,minuti,ore o giorni",
+                    type: "STRING",
+                    required: true,
+                    choices: [
+                        {
+                            name: "secondi",
+                            value: "secondi"
+                        },
+                        {
+                            name: "minuti",
+                            value: "minuti"
+                        },
+                        {
+                            name: "ore",
+                            value: "ore"
+                        },
+                        {
+                            name: "giorni",
+                            value: "giorni"
+                        }
+                    ]
+                },
+                {
+                    name: "motivo",
+                    description: "Perchè lo metti in timeout?",
+                    type: "STRING",
+                    required: false
+                }
+            ]
+        },
+        /*{
+            name: "clear",
+            description: "Elimina il numero di messaggi specificato",
+            options: [
+                {
+                    name: "numero",
+                    description: "Quanti messaggi vuoi eliminare",
+                    type: "INTEGER",
+                    required: true
+                }
+            ]
+        }*/
     ], guildId)
 
     Logger.Success("[UTIL] Created slash commands!")
